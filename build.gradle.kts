2aa426afa716506efdba3a4549544e1659e5de18
C32-2 <vasilybichutsky@gmail.com>
2025-09-23 13:37:26 +0300

    Added Dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,14 @@
 plugins {
     kotlin("jvm") version "2.0.21"
+    id("org.jetbrains.dokka") version "1.9.0"
 }
 
 group = "nvi.safe"
 version = "1.0-SNAPSHOT"
+
+tasks.dokkaHtml {
+    outputDirectory.set(layout.buildDirectory.dir("dokka"))
+}
 
 repositories {
     mavenCentral()
